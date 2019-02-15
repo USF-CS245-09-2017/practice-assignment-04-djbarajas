3745bbc3c209281ec41629235d0d8592c3c49512
djbarajas <35737585+djbarajas@users.noreply.github.com>
2019-02-15 13:42:22 -0800

    worked on method

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,3 +1,22 @@
 public class BubbleSort implements SortingAlgorithm{
 
+    private static void swap(int[] a, int i, int j){
+        int temp;
+        temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+    @Override
+    public void sort(int[] a) {
+        boolean swapped = true;
+        for(int j = 0;j<a.length-1 && swapped;j++) {//O(n)
+            swapped = false;
+            for (int i = 0; i < a.length - 1 - j; i++) {//O(n)
+                if (a[i] > a[i+1]) {
+                    swap(a, i, i + 1);
+                    swapped = true;
+                }
+            }
+        }
+    }
 }
--- SelectionSort.java
+++ SelectionSort.java
@@ -1,3 +1,26 @@
 public class SelectionSort implements SortingAlgorithm{
 
+    private static void swap(int[] a, int i, int j){
+        int temp;
+        temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    @Override
+    public void sort(int[] a) {
+        for(int i = 0; i < (a.length-1); i++){ //O(n)
+            swap(a, i,smallest(a,i)); //O(n^2)
+        }
+    }
+
+    private static int smallest(int[] a, int start){ //O(n)
+        int small = start;
+        for(int i = (start+1); i < a.length; i++){
+            if(a[i] < a[small]){
+                small = i;
+            }
+        }
+        return small;
+    }
 }
