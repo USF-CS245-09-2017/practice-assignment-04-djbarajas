af656c97228243b4c79c7f93071b54beebbbbb2c
djbarajas <35737585+djbarajas@users.noreply.github.com>
2019-02-16 13:59:50 -0800

    Commented insertion sort

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,4 +1,15 @@
 public class InsertionSort implements SortingAlgorithm {
+    /*
+    * insertion sort()
+    * sorts the values from least to greatest in an array
+    * Sets a marker for the first element in the array
+    * the left side is sorted and the right is the rest
+    * of the array. Selects the first unsorted element
+    * on the right side and adjust the elements in the
+    * sorted side.  The marker is then advanced by one
+    * element.  This continues until the entire array
+    * is sorted.
+     */
     @Override
     public void sort(int[] a) {
         int temp;
